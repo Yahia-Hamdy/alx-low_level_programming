797993bd8bb3173156a98474e32a98a08c6ee42f
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-15 21:02:11 +0000

    Q0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/*
+ * main - Entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("_putchar");
+	return (0);
+}
