4353721bae441f8eec33212c1e6b710e470126d5
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-16 21:12:44 +0000

    Q5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	printf("+\n");
+	_putchar("+");
 	return (1);
 	}
 	else if (n == 0)
 	{
-	printf("0\n");
+	_putchar("0");
 	return (0);
 	}
 	else
 	{
-	printf("-\n");
+	_putchar("-");
 	return (-1);
 	}
 }
