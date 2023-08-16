b871344b0cef3afb96d998055964a30105e3b906
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-16 20:42:25 +0000

    Q4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *_isalphar - checks if a letter  or not
+ *@c: check input of the function
+ *Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	return (1);
+	else
+	return (0);
+}
