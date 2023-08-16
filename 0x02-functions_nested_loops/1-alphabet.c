63dfbe3fba5f73e04ef23954473abdadef3586eb
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-16 18:19:14 +0000

    Q1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*main - Entry Point
+*
+*Description: Ptc to test betty
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char l;
+    for(l='a'; l<='z'; l++)
+        _putchar(l);
+    _putchar('\n');
+}
