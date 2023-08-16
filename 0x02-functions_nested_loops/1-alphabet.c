1bb4755ab7983a946435aa8346ca30b6b3cc157e
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-16 18:33:09 +0000

    Q1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,14 @@
 /**
 *main - Entry Point
 *
-*Description: Ptc to test betty
+*Description: prints letters for ffsy
 *
 *Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
 char l;
-    for(l='a'; l<='z'; l++)
-        _putchar(l);
-    _putchar('\n');
+	for (l = 'a'; l <= 'z'; l++)
+	_putchar(l);
+	_putchar('\n');
 }
