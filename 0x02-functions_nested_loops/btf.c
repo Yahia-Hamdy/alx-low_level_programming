8dab2f4de53080f3d36acd2431da471aab8a41fe
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-16 17:57:19 +0000

    Q0

---
+++ 0x02-functions_nested_loops/btf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry Point
+*
+*Description: Ptc to test betty
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Hello world!\n");
+	return (0);
+}
