24445f38dd1e1a7b393c388a9c7ace58eb885cf1
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-16 22:20:13 +0000

    Q10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *add - adds two integars
+ *@a: check input of the function
+ *@b: check input of the function
+ *Return: sum
+*/
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
