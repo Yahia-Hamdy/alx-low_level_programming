17e7ad2ff9734e5e6cf4248c05007f9c6046d848
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-16 22:56:42 +0000

    Q11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,23 @@
  *Return: 0 (success)
 */
 void print_to_98(int n)
+
+if (n < 98)
+{
 while (n < 98)
 {
 printf("%d, ", n);
 n = n + 1;
+}
+}
+
+else
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n = n - 1;
+}
 }
 printf("98\n");
 return (0);
