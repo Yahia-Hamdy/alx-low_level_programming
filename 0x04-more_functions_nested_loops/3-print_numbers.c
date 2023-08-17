5ea6a0db11ca13b906ff797927133d932b7ea1c1
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-17 18:48:42 +0000

    Q3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *print_numbers - prints from 0 till 9
+ *
+ *@i: counting var
+ *
+ *
+ *Return: void.
+*/
+
+void print_numbers(void)
+{
+for (int i = 0; i<=9; i++)
+	_putchar(i);
+_putchar("\n");
+}
