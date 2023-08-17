6e47154f5fe962e169ad3b950fe3ed854b1bc3d1
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-17 19:24:43 +0000

    Q3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
-#include <ctype.h>
 #include "main.h"
-
 /**
- *print_numbers - prints from 0 till 9
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  *
- *@i: counting var
- *
- *
- *Return: void.
-*/
-
+ */
 void print_numbers(void)
 {
-int i;
-for (i = 48; i < 58; i++)
-	_putchar(i);
-_putchar('\n');
+	int i;
+
+	for (i = 48; num < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
