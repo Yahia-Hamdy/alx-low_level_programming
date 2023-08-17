34dfda35e45f0c123ad6fb309a4006b281908674
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-17 18:39:57 +0000

    Q2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *mul - multiplies 2 integers
+ *
+ *@a: input var
+ *@b: input var
+ *
+ *Return: 1 if uppercase , 0 if not.
+*/
+
+int mul(int a, int b)
+{
+int sum;
+res = a * b;
+return (res);
+}
