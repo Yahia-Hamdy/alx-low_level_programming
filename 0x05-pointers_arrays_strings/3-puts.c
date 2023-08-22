78dcdca61164ac2db60b1e2d013752047cf97f9a
Yahia-Hamdy <yahiaa.hamdy@gmail.com>
2023-08-22 01:57:06 +0000

    Q3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - prints a string.
+ *
+ *@str: input string.
+ *
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+	_putchar(*str);
+	str++;
+	}
+
+}
